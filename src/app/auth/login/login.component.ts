1329dedf6b45f2d720c3553430323a19ee15877b
git.Ball <georgiballabanov@gmail.com>
2022-04-07 11:27:23 +0300

    Added login custom validations.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+loginFormGroup:FormGroup = this.formBuilder.group({
+'username':new FormControl('',[Validators.required,Validators.minLength(5)]),
+'password':new FormControl('',[Validators.required,Validators.minLength(5)]),
+});
+  constructor(
+    private formBuilder:FormBuilder,
+    private userService:UserService,
+    private router:Router,
+  ) { }
 
   ngOnInit(): void {
   }
+  loginHandler():void{
+// this.userService.login();
+// this.router.navigate(['/home']);
+console.log('form is submitted',this.loginFormGroup);
 
+  }
+  handleLogin():void{
+console.log('form must be submitted');
+
+  }
+  onSubmit():void{
+    // if(this.recipeName.errors){
+    //   // this.recipeName.invalid;
+    //   console.log('recipeName')
+  
+    // }
+    // if(this.ingredients.errors){
+    //   // this.ingredients.invalid;
+  
+    //   console.log('ingredients')
+  
+    // }
+    // if(this.preparation.errors){
+    //   // this.preparation.invalid;
+    //   // this.preparation.touched;
+  
+    //   console.log('preparation')
+  
+    // }
+  }
 }
