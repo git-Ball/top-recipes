b2b9410802b7d254fa30ee3376ff0854cb5ac419
git.Ball <georgiballabanov@gmail.com>
2022-04-08 18:09:06 +0300

    Added Register form validations.

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  get passwordGroup():FormGroup{
+    return this.registerFormGroup.controls['password'] as FormGroup;
+  }
+  registerFormGroup: FormGroup = this.formBuilder.group({
+    'username': new FormControl('', [Validators.required, Validators.minLength(5)]),
+    'passwords': new FormControl({
+      'password': new FormControl('', [Validators.required, Validators.minLength(5)]),
+      //TODO pass == repass
+      'repass': new FormControl(),
+    }),
+
+
+  })
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
-
+  handleRegister():void{
+    
+  }
 }
---
+++ src/app/auth/util.ts
@@ -0,0 +1,13 @@
+// import { AbstractControl } from "@angular/forms";
+
+// export function passwordMatch(passwordFormControl: AbstractControl) {
+//     return (repassFormControl: AbstractControl) => {
+//         if (passwordFormControl.value !== repassFormControl.value) {
+//             return {
+//                  passwordMatch: true 
+//                 }
+//         }
+//         return null;
+//     }
+// }
+
